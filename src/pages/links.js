2dc72eed3971ce33663ae9a70cf95100ba9f330d
Kassio <kassiogluten@gmail.com>
2021-09-06 23:50:35 -0300

    fix: last build

--- src/pages/links.js
+++ src/pages/links.js
@@ -1,8 +1,6 @@
 import Head from "next/head";
-import { Contact } from "../components/Contact";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
-import { LinksPage } from "../components/links/Links";
 import { List } from "../components/links/List";
 
 export default function HomeLinks() {
